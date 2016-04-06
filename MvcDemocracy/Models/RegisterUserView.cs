b5f3b55e13da3360ac2da7d89f6ecd6c8cb75c82
emilio barrera <barrera_emilio@hotmail.com>
2016-04-06 18:11:32 +0200

    RegisterUserView

---
+++ MvcDemocracy/Models/RegisterUserView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcDemocracy.Models
+{
+    public class RegisterUserView : UserView
+    {
+        [Display(Name ="Password")]
+        [Required(ErrorMessage = "The field {0} is required.")]
+        [StringLength(20, ErrorMessage = "The field {0} can contain {1} and minimum {2} caracters", MinimumLength = 8 )]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [Required(ErrorMessage = "The field {0} is required.")]
+        [StringLength(20, ErrorMessage = "The field {0} can contain {1} and minimum {2} caracters", MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password and confirm does not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
